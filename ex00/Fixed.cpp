ad40f6dff53dad1c69a0cf426c5cbcc76fca836a
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-12-27 18:15:58 +0900

    modified: コードの順番を修正

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: yonuma <yonuma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 12:52:36 by yonuma            #+#    #+#             */
-/*   Updated: 2025/12/27 15:16:38 by yonuma           ###   ########.fr       */
+/*   Updated: 2025/12/27 18:15:43 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,6 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 }
 
-int	Fixed::getRawBits(void)	const
-{
-	std::cout << "getRawBits member function called" << std::endl;
-	return fixed_point_number_;
-}
-
-void	Fixed::setRawBits(int const raw)
-{
-	fixed_point_number_ = raw;
-}
-
 Fixed::Fixed(const Fixed& copy)
 {
 	std::cout << "Copy constructor called" << std::endl;
@@ -45,4 +34,15 @@ Fixed&	Fixed::operator=(const Fixed& other)
 	if (this != &other)
 		fixed_point_number_ = other.getRawBits();
 	return *this;
+}
+
+int	Fixed::getRawBits(void)	const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return fixed_point_number_;
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	fixed_point_number_ = raw;
 }
